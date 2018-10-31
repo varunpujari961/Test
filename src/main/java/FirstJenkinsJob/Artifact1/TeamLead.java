c70616a6d45f59657d98d06622690b9d97ecdbbc
Varun Pujari <varun.kumar.pujari@accenture.com>
2018-10-31 14:54:54 +0530

    First Commit

---
+++ src/main/java/FirstJenkinsJob/Artifact1/TeamLead.java
@@ -0,0 +1,14 @@
+package FirstJenkinsJob.Artifact1;
+
+
+public class TeamLead extends Employee{
+	private int numberOfProjectsManaged;
+
+	public int getNumberOfProjectsManaged() {
+		return numberOfProjectsManaged;
+	}
+	public void setNumberOfProjectsManaged(int numberOfProjectsManaged) {
+		this.numberOfProjectsManaged = numberOfProjectsManaged;
+	}	
+}
+
